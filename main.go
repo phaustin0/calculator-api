7c9367fc0abed174c6233e60bf28aa43889131ff
phaustin0 <zechy14@icloud.com>
2024-10-30 23:54:36 +0800

    set up routes for api

--- main.go
+++ main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"net/http"
+)
+
+func handleRoutes() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /add", add)
+	mux.HandleFunc("POST /subtract", subtract)
+	mux.HandleFunc("POST /multiply", multiply)
+	mux.HandleFunc("POST /divide", divide)
+	mux.HandleFunc("POST /sum", sum)
+	return mux
+}
+
 func main() {
+	routeHandler := handleRoutes()
+
+	server := &http.Server{
+		Addr:    ":8000",
+		Handler: routeHandler,
+	}
 }
---
+++ routes.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"net/http"
+)
+
+func add(w http.ResponseWriter, r *http.Request) {}
+
+func subtract(w http.ResponseWriter, r *http.Request) {}
+
+func multiply(w http.ResponseWriter, r *http.Request) {}
+
+func divide(w http.ResponseWriter, r *http.Request) {}
+
+func sum(w http.ResponseWriter, r *http.Request) {}
